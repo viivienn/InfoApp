a08443f1adcff4d50d9118065fb9ec94f66d13fc
evgeni <evgeni@heyday.ai>
2020-05-04 02:09:41 -0400

    first commit

---
+++ app/src/main/java/com/example/myapplication/ui/language/LanguageFragment.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.ui.language
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class LanguageFragment : Fragment()  {
+//    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        homeViewModel =
+//            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_language, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentSettingsBinding
+import com.example.myapplication.ui.language.LanguageFragment
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val binding: FragmentSettingsBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_settings, container, false
+        )
+        binding.languagesOption.setOnClickListener { view: View ->
+            view.findNavController().navigate(SettingsFragmentDirections.actionNavigationSettingsToLanguageFragment2())
+        }
+
+        return binding.root
+    }
+}
